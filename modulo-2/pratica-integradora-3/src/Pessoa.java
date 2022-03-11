d82eda1f91cc0cdf52d26eb2787e00f0f8d893dc
Weverton Bruno <weverton.ssantos@mercadolivre.com>
2022-03-11 16:31:46 -0300

    feat: Exercicio 2

--- modulo-2/pratica-integradora-3/src/Pessoa.java
+++ modulo-2/pratica-integradora-3/src/Pessoa.java
@@ -4,4 +4,20 @@ public class Pessoa {
     private Integer idade;
     private Float peso;
     private Float altura;
+
+    public Pessoa(){}
+
+    public Pessoa(String ID, String nome, Integer idade){
+        this.ID = ID;
+        this.idade = idade;
+        this.nome = nome;
+    }
+
+    public Pessoa(String ID, String nome, Integer idade, Float peso, Float altura) {
+        this.ID = ID;
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+    }
 }
