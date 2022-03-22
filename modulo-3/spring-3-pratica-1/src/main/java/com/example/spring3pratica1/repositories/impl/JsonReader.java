ee0304a34099a234ab2b72493b3185bc2063a1c6
Weverton Bruno <weverton.ssantos@mercadolivre.com>
2022-03-22 17:16:56 -0300

    chore: Add documentacao para o JsonReader

--- modulo-3/spring-3-pratica-1/src/main/java/com/example/spring3pratica1/repositories/impl/JsonReader.java
+++ modulo-3/spring-3-pratica-1/src/main/java/com/example/spring3pratica1/repositories/impl/JsonReader.java
@@ -5,11 +5,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URL;
 
+/**
+ * Util class to handle Json files
+ */
 public class JsonReader {
-    public static <T> T read(String path, Class<T> type) throws IOException {
+
+    /**
+     * Reads a json file and returns a java object instance of type 'classType'
+     * @param path
+     * @param classType
+     * @param <T>
+     * @return
+     * @throws IOException
+     */
+    public static <T> T read(String path, Class<T> classType) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         return objectMapper.readValue(
-                new URL(path), type);
+                new URL(path), classType);
     }
 }
